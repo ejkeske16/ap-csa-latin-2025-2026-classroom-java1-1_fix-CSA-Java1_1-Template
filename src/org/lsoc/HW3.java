5a1b381dbb9bda788a6f2f0cd7032d399da76da2
Elliot K. <ekeske@lsoc.org>
2025-09-05 19:11:26 +0000

    I assigned values to each num, then did an addition system and printed it, then made a similar one for the average. I also printed some spaces in between so you can see the two numbers.

---
+++ src/org/lsoc/HW3.java
@@ -0,0 +1,12 @@
+public class HW3 {
+  public static void main (String [] args){
+    int num1 = 475;
+    int num2 = 821;
+    int num3 = 369;
+    int num4 = 562;
+    int num5 = num1 + num2 + num3 + num4;
+    System.out.print(num5 + " ");
+    int num6 = (num1 + num2 + num3 + num4) / 4;
+    System.out.print(num6);
+  } 
+}
